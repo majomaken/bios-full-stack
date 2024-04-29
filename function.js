8cd656f37ed741db621b70f81a895f4d23e54f65
majomaken <majo.maken@gmail.com>
2024-04-29 06:26:46 -0500

    Add classes coercion, function and operators

---
+++ function.js
@@ -0,0 +1,21 @@
+//Scope
+var funcion = function() {};
+let global = 'global';
+
+function suma (parametro1) {
+  parametro1 = 'local'
+}
+
+//Functions
+
+function suma (parametro1, parametro2) {}
+
+var resta = function (parametro1, parametro2) {
+  console.log(parametro1 - parametro2)
+}
+
+const multiplicacion = (parametro1, parametro2) => {
+  return console.log(parametro1 * parametro2)
+}
+
+console.log(multiplicacion(3, 2))
