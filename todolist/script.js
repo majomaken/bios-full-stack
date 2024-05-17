b1abf9272e32085ab10c9903eb6e26ae162c8638
Miguel Jimenez <40671304+majomaken@users.noreply.github.com>
2024-05-17 06:41:23 -0500

    Revert "feat: Add changes to 'toDo list' styles"

--- todolist/script.js
+++ todolist/script.js
@@ -2,26 +2,22 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 const addTask = () => {
-    const task = inputBox.value.trim();
-    if (!task){
-        alert('Por favor ingreseuna tarea!!')
-        console.log('Esto no es una tarea')
-    
-    return;
-    }
-    
-    const li = document.createElement('li');
-    li.innerHTML = `
-    <div class="tasks-container">
-        <label>
-            <input type="checkbox" />
-            <span>${task}</span>
-        </label>
-        <div class="buttons-container">
-            <button class="edit-btn">Editar</button>
-            <button class="delete-btn">Eliminar</button>
-        </div>
-    </div>
-    `;
-    listContainer.appendChild(li);
+  const task = inputBox.value.trim();
+  if (!task) {
+    alert('Por favor ingrese una tarea!!!')
+    console.log('Esto no es una tarea')
+  }
+
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <label for="check">
+      Este es el check
+      </label>
+      <input id="check" name="check" type="checkbox" />
+    <span>${task}</span>
+    <button class="edit-btn">Editar</button>
+    <button class="delete-btn">Eliminar</button>
+  `;
+
+  listContainer.appendChild(li);
 }
